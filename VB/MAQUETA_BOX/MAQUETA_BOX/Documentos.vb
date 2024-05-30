16484cdfaa62fca03bf02899ed3424e4794fa4ad
Augusto093 <augustoelkgt@gmail.com>
2024-05-30 15:21:40 -0300

    Documentos 2

--- VB/MAQUETA_BOX/MAQUETA_BOX/Documentos.vb
+++ VB/MAQUETA_BOX/MAQUETA_BOX/Documentos.vb
@@ -1,3 +1,39 @@
-﻿Public Class Documentos
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+Public Class Documentos
 
+    Dim conexion As SqlConnection
+    Dim comando As New SqlCommand
+
+#Region "Llenar Grilla"
+
+    Private Sub LlenarGrilla()
+
+        conexion = New SqlConnection("data source = 168.197.51.109; initial catalog = PIN_GRUPO11 ; user id = PIN_GRUPO11; password = PIN_GRUPO11123")
+
+        conexion.Open()
+        comando.Connection = conexion
+        comando.CommandType = CommandType.StoredProcedure
+        comando.CommandText = ("") 'falta el procedimiento
+
+        Dim datadapter As New SqlDataAdapter(comando)
+        Dim oDs As New DataSet
+        datadapter.Fill(oDs)
+
+        If oDs.Tables(0).Rows.Count > 0 Then
+            DataGridView1.AutoGenerateColumns = True
+            DataGridView1.DataSource = oDs.Tables(0)
+            DataGridView1.Refresh()
+        End If
+        oDs = Nothing
+        conexion.Close()
+
+    End Sub
+
+#End Region
+
+
+    Private Sub Documentos_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
