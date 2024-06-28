6d2bacd45e786a832bd0ec27df51ca576059669f
Elkyat <gaiteaugusto1@gmail.com>
2024-06-28 04:17:39 -0300

    EXPORT TO EXCEL INSANOO EN DOCS
    Le faltan cosas como exportar por filtro o algo asi

--- VB/MAQUETA_BOX/MAQUETA_BOX/Documentos.Designer.vb
+++ VB/MAQUETA_BOX/MAQUETA_BOX/Documentos.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Documentos
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
@@ -32,7 +33,17 @@ Partial Class Documentos
         Me.cboProveedores = New System.Windows.Forms.ComboBox()
         Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
         Me.btnExportar = New System.Windows.Forms.Button()
+        Me.PIN_GRUPO11DataSet = New MAQUETA_BOX.PIN_GRUPO11DataSet()
+        Me.PINGRUPO11DataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ConsultardocumentosOrComprasBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Consultar_documentos_OrComprasTableAdapter = New MAQUETA_BOX.PIN_GRUPO11DataSetTableAdapters.consultar_documentos_OrComprasTableAdapter()
+        Me.ConsultardocumentosOrVentasBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Consultar_documentos_OrVentasTableAdapter = New MAQUETA_BOX.PIN_GRUPO11DataSetTableAdapters.consultar_documentos_OrVentasTableAdapter()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PIN_GRUPO11DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PINGRUPO11DataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ConsultardocumentosOrComprasBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ConsultardocumentosOrVentasBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DataGridView1
@@ -129,6 +140,34 @@ Partial Class Documentos
         Me.btnExportar.Text = "Exportar"
         Me.btnExportar.UseVisualStyleBackColor = True
         '
+        'PIN_GRUPO11DataSet
+        '
+        Me.PIN_GRUPO11DataSet.DataSetName = "PIN_GRUPO11DataSet"
+        Me.PIN_GRUPO11DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'PINGRUPO11DataSetBindingSource
+        '
+        Me.PINGRUPO11DataSetBindingSource.DataSource = Me.PIN_GRUPO11DataSet
+        Me.PINGRUPO11DataSetBindingSource.Position = 0
+        '
+        'ConsultardocumentosOrComprasBindingSource
+        '
+        Me.ConsultardocumentosOrComprasBindingSource.DataMember = "consultar_documentos_OrCompras"
+        Me.ConsultardocumentosOrComprasBindingSource.DataSource = Me.PINGRUPO11DataSetBindingSource
+        '
+        'Consultar_documentos_OrComprasTableAdapter
+        '
+        Me.Consultar_documentos_OrComprasTableAdapter.ClearBeforeFill = True
+        '
+        'ConsultardocumentosOrVentasBindingSource
+        '
+        Me.ConsultardocumentosOrVentasBindingSource.DataMember = "consultar_documentos_OrVentas"
+        Me.ConsultardocumentosOrVentasBindingSource.DataSource = Me.PINGRUPO11DataSetBindingSource
+        '
+        'Consultar_documentos_OrVentasTableAdapter
+        '
+        Me.Consultar_documentos_OrVentasTableAdapter.ClearBeforeFill = True
+        '
         'Documentos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -146,6 +185,10 @@ Partial Class Documentos
         Me.Name = "Documentos"
         Me.Text = "Documentos"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PIN_GRUPO11DataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PINGRUPO11DataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ConsultardocumentosOrComprasBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ConsultardocumentosOrVentasBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -156,4 +199,10 @@ Partial Class Documentos
     Friend WithEvents cboProveedores As System.Windows.Forms.ComboBox
     Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
     Friend WithEvents btnExportar As Button
+    Friend WithEvents PINGRUPO11DataSetBindingSource As BindingSource
+    Friend WithEvents PIN_GRUPO11DataSet As PIN_GRUPO11DataSet
+    Friend WithEvents ConsultardocumentosOrComprasBindingSource As BindingSource
+    Friend WithEvents Consultar_documentos_OrComprasTableAdapter As PIN_GRUPO11DataSetTableAdapters.consultar_documentos_OrComprasTableAdapter
+    Friend WithEvents ConsultardocumentosOrVentasBindingSource As BindingSource
+    Friend WithEvents Consultar_documentos_OrVentasTableAdapter As PIN_GRUPO11DataSetTableAdapters.consultar_documentos_OrVentasTableAdapter
 End Class
